1402f2533f426eba38b235a8016125ee7bc88acd
elviskankola1 <elviskankola1@gmail.com>
2019-01-12 00:11:39 +0200

    new css and js for vue

---
+++ application/views/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>cyan Flat ui kit Website Template | Home :: w3layouts</title>
+		<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+		<script src="js/jquery.min.js"></script>
+		<link href="css/style.css" rel='stylesheet' type='text/css' />
+   		<link rel="stylesheet" href="fonts/css/font-awesome.min.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		</script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="top-header">
+				<link type="text/css" rel="stylesheet" href="css/jquery.mmenu.all.css" />
+				<script type="text/javascript" src="js/jquery.mmenu.js"></script>
+				<script type="text/javascript">
+					$(function() {
+						$('nav#menu-left').mmenu();
+					});
+				</script>
+				<div id="page">
+					<div id="header">
+						<a href="#menu-left"> </a>
+					</div>
+					<nav id="menu-left">
+						<ul>
+							<li><a href="#">HOME</a></li>
+							<li class="active"><a href="#">NOUVELLES</a></li>
+							<li><a href="#">About</a></li>
+						</ul>
+					</nav>
+				</div>
+				<div class="logo">
+					<span>Dashboard </span>
+				</div>
+				<div class="usernotifications">
+					<ul class="user-profile list-unstyled">
+						<li><a href="#"><img src="images/user-pic.jpg" title="admin" /></a>
+							<ul class="sub list-unstyled">
+								<li><a href="#">Profile</a></li>
+								<li><a href="#">Settings</a></li>
+								<li><a href="#">custom</a></li>
+							</ul>
+						</li>
+					</ul>
+					<ul class="notification list-unstyled user-profile">
+						<li><a href="#"><img src="images/notification-icon.png" title="notifications" /></a>
+							<ul class="sub list-unstyled">
+								<li><a href="#">20</a></li>
+							</ul>
+						</li>
+					</ul>
+					<ul class="logout list-unstyled">
+						<li><a href="#"><span> </span></a></li>
+					</ul>
+				</div>
+				<div class="clearfix"> 33</div>
+			</div>
+			<div class="clearfix"> </div>
+			<div class="content">
+				<div class="3-cols">
+					<div class="col-1 col-md-3">
+							<div class="user-profile1 text-center">
+								<img src="images/people.png" title="name" />
+								<h3>Zach Dunes</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus dui id libero auctor cursus. </p>
+								<a class="p-btn" href="#">Profile</a>
+							</div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+
