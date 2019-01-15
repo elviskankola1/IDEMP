44d89596cc16ae98f13a181138ca910ebecb2f7c
elviskankola1 <elviskankola1@gmail.com>
2019-01-15 14:07:00 +0200

    new classpour la validation par reconaissance faciale

---
+++ application/libraries/FacialValidate.php
@@ -0,0 +1,44 @@
+<?php
+
+    class FacialValidate{
+        private $image_user;
+        private $images_compare;
+
+        function __construct($image_user,$image_compare){
+            
+            $this->image_user = $image_compare;
+            $this->images_compare = $image_compare;
+        }
+
+        protected function ReCreateImage($image){
+
+            $id_image = imagecreatefromjpeg($image);
+            return $id_image;
+
+        }
+
+        protected function ClearImage($id_image){
+
+        }
+
+        protected function DeleteImageInMemory($image){
+            $destroy = imagedestroy($image);
+            return $destroy;
+        }
+
+        protected function getImageCompare(){
+
+            return $this->images_compare;
+        }
+
+        protected function getImageUser(){
+
+            return $this->image_user;
+        }
+
+        protected function SendToNavigator($image){
+
+            header('Content-Type: image/png');
+            imagepng($image);
+        }
+    }
